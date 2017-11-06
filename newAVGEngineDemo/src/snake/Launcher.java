d49486cf716a761a334cc8fa3d98eaa059874f0d
snw2015 <4564959@qq.com>
2017-11-06 20:48:23 +0900

    snake fix
    speed improve

--- newAVGEngineDemo/src/snake/Launcher.java
+++ newAVGEngineDemo/src/snake/Launcher.java
@@ -12,79 +12,67 @@ import snw.engine.database.Database;
 import snw.engine.main.MainFrame;
 
 public class Launcher {
-	public final static int fps = 10;
-	public final static int gcInterval = 5;
-	public static int counter = 0;
+    public final static int fps = 10;
+    public final static int gcInterval = 10;
+    public static int counter = 0;
 
-	public static void main(String[] s) {
-		Database.loadUserData();
+    public static void main(String[] s) {
+        Database.loadUserData();
 
-		MainFrame frame = new MainFrame("Snake");
-		SnakeScreenDemo panel = new SnakeScreenDemo("panel", 0, 0, 500, 500, 20, 20, frame);
-		frame.setMainPanel(panel);
-		SnakeProcessor processor = new SnakeProcessor(20, 20, panel);
-		processor.initialize();
-		frame.addKeyListener(new KeyListener() {
+        MainFrame frame = new MainFrame("Snake");
+        SnakeScreenDemo panel = new SnakeScreenDemo("panel", 0, 0, 500, 500, 20, 20, frame);
+        frame.setMainPanel(panel);
+        SnakeProcessor processor = new SnakeProcessor(20, 20, panel);
+        processor.initialize();
+        frame.addKeyListener(new KeyListener() {
 
-			@Override
-			public void keyTyped(KeyEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
 
-			}
+            }
 
-			@Override
-			public void keyPressed(KeyEvent e) {
-				Direction direction = Direction.LEFT;
+            @Override
+            public void keyPressed(KeyEvent e) {
+                Direction direction = Direction.LEFT;
 
-				switch (e.getKeyCode()) {
-				case KeyEvent.VK_LEFT:
-					direction = Direction.LEFT;
-					break;
-				case KeyEvent.VK_RIGHT:
-					direction = Direction.RIGHT;
-					break;
-				case KeyEvent.VK_UP:
-					direction = Direction.UP;
-					break;
-				case KeyEvent.VK_DOWN:
-					direction = Direction.DOWN;
-				}
-				processor.changeDirection(direction);
-			}
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        direction = Direction.LEFT;
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        direction = Direction.RIGHT;
+                        break;
+                    case KeyEvent.VK_UP:
+                        direction = Direction.UP;
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        direction = Direction.DOWN;
+                }
+                processor.changeDirection(direction);
+            }
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
 
-			}
-		});
+            }
+        });
 
-		Timer timerPaint = new Timer(1000 / fps, (ActionEvent e) -> {
-			if (frame.isRunning) {
-				frame.repaint();
+        Timer timerPaint = new Timer(1000 / fps, (ActionEvent e) -> {
+            if (frame.isRunning) {
+                frame.repaint();
+            }
+        });
+        timerPaint.start();
 
-				if (counter++ >= gcInterval) {
-					System.gc();
+        Timer timerProcess = new Timer(1000 / fps, (ActionEvent e) -> {
+            if (frame.isRunning) {
+                processor.process();
+            }
+        });
+        timerProcess.start();
 
-					counter = 0;
-				}
-			}
-		});
-		timerPaint.start();
-
-		Timer timerProcess = new Timer(1000 / fps, (ActionEvent e) -> {
-			if (frame.isRunning) {
-				processor.process();
-
-				if (counter++ >= gcInterval) {
-					System.gc();
-
-					counter = 0;
-				}
-			}
-		});
-		timerProcess.start();
-
-		frame.start();
-	}
+        frame.start();
+    }
 }
